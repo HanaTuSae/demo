5a98ad113256ad40e218bf9c302abdbe616c0f76
HanaTuSae <zhengdingdong0307@qq.com>
2016-10-14 19:08:08 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,7 @@
  * @date    2016-10-10 19:47:47
  * @version $Id$
  */
-
+windo.onload=function(){
 myFocus.set({id:'banner',pattern: 'mF_kdui',time: 3,trigger: 'mouseover',delay: 200,txtHeight: '0'});
+}
+
